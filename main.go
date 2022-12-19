7cf1b60b8e8de9d002b10f4e28090862cf76b608
FeifanW <qihang791@163.com>
2022-12-20 00:17:09 +0800

    练习一些运算符

--- main.go
+++ main.go
@@ -1,41 +1,25 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
+import "fmt"
 
 func main() {
-	// 案例1：字符串转布尔值
-	var str string = "true"
-	var b bool
-	/*
-		b, _ = strconv.ParseBool(str)
-		说明
-		1.strconv.ParseBool(str) 函数会返回两个值 （value bool, err error)
-		2.因为我只想获取到 value bool, 不想获取 err 所以我使用_忽略
-	*/
+	//// 基本数据类型在内存布局
+	//var i int = 10
+	//// i的地址是什么，&i
+	//fmt.Println("i的地址=", &i)
+	//
+	//// 下面的var ptr *int = &i
+	//// ptr是一个指针变量
+	//// ptr的类型 *int
+	//// ptr 本身的值&i
+	//var ptr *int = &i
+	//fmt.Printf("ptr=%v\n", ptr)
 
-	b, _ = strconv.ParseBool(str)
-	fmt.Printf("b type %T b=%v\n", b, b)
+	var num int = 9
+	fmt.Printf("num address=%v\n", &num)
 
-	// 案例2：字符串转int
-	var str2 string = "12345690"
-	var n1 int64
-	var n2 int
-	n1, _ = strconv.ParseInt(str2, 10, 64) // 返回的是int64，想要得到int32需要再转一下
-	n2 = int(n1)
-	fmt.Printf("n1 type %T n1=%v\n", n1, n1)
-	fmt.Printf("n2 type %T n2=%v\n", n2, n2)
-
-	// 案例3：字符串转float
-	var str3 string = "123.456"
-	var f1 float64
-	f1, _ = strconv.ParseFloat(str3, 64)
-	fmt.Printf("f1 type %T f1=%v\n", f1, f1)
-
-	var str4 string = "hello"
-	var n3 int64
-	n3, _ = strconv.ParseInt(str4, 10, 64)
-	fmt.Printf("n3 type %T n3=%v\n", n3, n3)
+	var ptr *int
+	ptr = &num
+	*ptr = 10
+	fmt.Println("num =", num)
 }
