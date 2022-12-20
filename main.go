f09cb1b3efcdf74f6d2f4a8d17bd6e13d359c341
FeifanW <qihang791@163.com>
2022-12-21 00:19:01 +0800

    练习输入输出

--- main.go
+++ main.go
@@ -15,11 +15,24 @@ func main() {
 	//var ptr *int = &i
 	//fmt.Printf("ptr=%v\n", ptr)
 
-	var num int = 9
-	fmt.Printf("num address=%v\n", &num)
+	var name string
+	var age byte
+	var sal float32
+	var isPass bool
 
-	var ptr *int
-	ptr = &num
-	*ptr = 10
-	fmt.Println("num =", num)
+	// 方法1
+	fmt.Println("请输入姓名")
+	fmt.Scanln(&name)
+	fmt.Println("请输入年龄")
+	fmt.Scanln(&age)
+	fmt.Println("请输入薪水")
+	fmt.Scanln(&sal)
+	fmt.Println("请输入是否通过考试")
+	fmt.Scanln(&isPass)
+
+	fmt.Printf("名字是 %v \n 年龄是 %v \n 薪水是 %v \n 是否通过考试 %v \n", name, age, sal, isPass)
+	//// 方法2
+	fmt.Println("请输入你的姓名，年龄，薪水，是否通过考试，使用空格隔开")
+	fmt.Scanf("%s %d %f %t", &name, &age, &sal, &isPass)
+	fmt.Printf("名字是 %v \n 年龄是 %v \n 薪水是 %v \n 是否通过考试 %v \n", name, age, sal, isPass)
 }
