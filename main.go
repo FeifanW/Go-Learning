80664995e127df2641db836f20479c4681bfba06
FeifanW <qihang791@163.com>
2022-12-22 01:02:28 +0800

    练习分支

--- main.go
+++ main.go
@@ -3,36 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	//// 基本数据类型在内存布局
-	//var i int = 10
-	//// i的地址是什么，&i
-	//fmt.Println("i的地址=", &i)
-	//
-	//// 下面的var ptr *int = &i
-	//// ptr是一个指针变量
-	//// ptr的类型 *int
-	//// ptr 本身的值&i
-	//var ptr *int = &i
-	//fmt.Printf("ptr=%v\n", ptr)
-
-	var name string
-	var age byte
-	var sal float32
-	var isPass bool
-
-	// 方法1
-	fmt.Println("请输入姓名")
-	fmt.Scanln(&name)
-	fmt.Println("请输入年龄")
+	var age int
+	fmt.Println("请输入年龄：")
 	fmt.Scanln(&age)
-	fmt.Println("请输入薪水")
-	fmt.Scanln(&sal)
-	fmt.Println("请输入是否通过考试")
-	fmt.Scanln(&isPass)
-
-	fmt.Printf("名字是 %v \n 年龄是 %v \n 薪水是 %v \n 是否通过考试 %v \n", name, age, sal, isPass)
-	//// 方法2
-	fmt.Println("请输入你的姓名，年龄，薪水，是否通过考试，使用空格隔开")
-	fmt.Scanf("%s %d %f %t", &name, &age, &sal, &isPass)
-	fmt.Printf("名字是 %v \n 年龄是 %v \n 薪水是 %v \n 是否通过考试 %v \n", name, age, sal, isPass)
+	if age > 18 {
+		fmt.Println("你的年龄大于18，要对自己的行为负责")
+	}
 }
