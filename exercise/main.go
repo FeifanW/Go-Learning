0468a56e1274cbb3251aee4fbc819c6408fc8246
FeifanW <qihang791@163.com>
2023-02-12 17:43:56 +0800

    练习go类型断言

--- exercise/main.go
+++ exercise/main.go
@@ -32,6 +32,27 @@ func (hs HeroSlice) Swap(i, j int) {
 	//hs[j] = temp
 	hs[i], hs[j] = hs[j], hs[i]
 }
+
+// 编写一个函数，判断输入的参数是什么类型
+func TypeJudge(items ...interface{}) {
+	for index, x := range items {
+		switch x.(type) {
+		case bool:
+			fmt.Printf("第%v个参数是bool类型，值是%v\n", index, x)
+		case float32:
+			fmt.Printf("第%v个参数是float32类型，值是%v\n", index, x)
+		case float64:
+			fmt.Printf("第%v个参数是float64类型，值是%v\n", index, x)
+		case int, int32, int64:
+			fmt.Printf("第%v个参数是整数类型，值是%v\n", index, x)
+		case string:
+			fmt.Printf("第%v个参数是string类型，值是%v\n", index, x)
+		default:
+			fmt.Printf("第%v个参数是类型不确定，值是%v\n", index, x)
+		}
+	}
+}
+
 func main() {
 	// 先定义一个数组/切片
 	var intSlice = []int{0, -1, 10, 7, 90}
@@ -39,26 +60,48 @@ func main() {
 	// 1.冒泡排序
 	// 2.也可以使用系统提供的方法
 	sort.Ints(intSlice)
-	fmt.Println(intSlice)
+	//fmt.Println(intSlice)
 
 	// 测试看看我们是否可以对结构体切片进行排序
 	var heroes HeroSlice
 	for i := 0; i < 10; i++ {
 		hero := Hero{
-			Name: fmt.Sprintf("英雄~%d", rand.Intn(100)),
-			Age:  rand.Intn(100),
+			//Name: fmt.Sprintf("英雄~%d", rand.Intn(100)),
+			Age: rand.Intn(100),
 		}
 		//将hero append到heroes切片
 		heroes = append(heroes, hero)
 	}
 	// 看看排序前的顺序
-	for _, v := range heroes {
-		fmt.Println(v)
-	}
+	//for _, v := range heroes {
+	//	fmt.Println(v)
+	//}
 	//调用sort.Sort
 	sort.Sort(heroes)
 	// 看看排序后的顺序
-	for _, v := range heroes {
-		fmt.Println(v)
-	}
+	//for _, v := range heroes {
+	//	fmt.Println(v)
+	//}
+
+	//var usbArr = [3]Usb
+	//usbArr[0] = Phone{}
+	//usbArr[1] = Phone{}
+	//usbArr[2] = Camera{}
+	//fmt.Println(usbArr)
+
+	// 类型断言的其他案例
+	var x interface{}
+	var b float32 = 1.1
+	x = b // 空接口，可以接收任意类型
+	// x => float32 [使用类型断言]
+	y := x.(float32)
+	fmt.Printf("y的类型是%T 值是%v", y, y)
+
+	var n1 float32 = 1.1
+	var n2 float64 = 2.3
+	var n3 int32 = 30
+	var name string = "tom"
+	address := "北京"
+	n4 := 300
+	TypeJudge(n1, n2, n3, name, address, n4)
 }
